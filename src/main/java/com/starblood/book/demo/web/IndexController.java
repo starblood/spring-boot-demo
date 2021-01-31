390356e823490c6f4389e8aa0e269506fb0cf8b4
john.plto <john.plto@kakaoenterprise.com>
2021-01-31 22:13:17 +0900

    add oauth2 for naver authentication

--- src/main/java/com/starblood/book/demo/web/IndexController.java
+++ src/main/java/com/starblood/book/demo/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.starblood.book.demo.web;
 
+import com.starblood.book.demo.config.auth.LoginUser;
 import com.starblood.book.demo.config.auth.dto.SessionUser;
 import com.starblood.book.demo.service.posts.PostsService;
 import com.starblood.book.demo.web.dto.PostsResponseDto;
@@ -18,9 +19,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
