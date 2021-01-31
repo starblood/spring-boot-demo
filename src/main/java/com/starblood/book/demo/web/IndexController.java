35a57ad6419ec332881bbb7901b41e92ce84951f
john.plto <john.plto@kakaoenterprise.com>
2021-01-31 20:33:53 +0900

    add OAuth2 authentication

--- src/main/java/com/starblood/book/demo/web/IndexController.java
+++ src/main/java/com/starblood/book/demo/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.starblood.book.demo.web;
 
+import com.starblood.book.demo.config.auth.dto.SessionUser;
 import com.starblood.book.demo.service.posts.PostsService;
 import com.starblood.book.demo.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +9,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
